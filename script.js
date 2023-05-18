2938ae173a59add5bacd52da4ac99794ec8751e9
Artem <artemspravnikov77@gmail.com>
2023-05-18 21:56:51 +0300

    Third commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:false,
+        adaptiveHeight:true,
+        autoplay:true,
+        autoplaySpeed:4000,
+        centerMode:true,
+        fade:true,
+        pauseOnHover:false,
+    });
+});
+

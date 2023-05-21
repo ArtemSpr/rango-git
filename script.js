d2448976965d7e72f704de197e5b5a8e69ef236a
Artem <artemspravnikov77@gmail.com>
2023-05-21 17:50:14 +0300

    Add new elment

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
     $('.slider').slick({
-        arrows:false,
+        arrow:false;
+        dots:false,
+        adaptiveHeight:true,
+        autoplay:true,
+        autoplaySpeed:4000,
+        centerMode:true,
+        fade:true,
+        pauseOnHover:false,
+    });
+});
+
+$(document).ready(function(){
+    $('.secSlider').slick({
+        arrows:true,
         dots:false,
         adaptiveHeight:true,
         autoplay:true,
